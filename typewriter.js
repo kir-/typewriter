12c00a2573bcf9e3f289047c9889a578e84b0844
kir- <graysonclee@gmail.com>
2019-07-02 20:30:15 +0000

    finished

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+let x = 0;
+
+let sentenceModifier = sentence + '\n';
+for (const char of sentenceModifier) {
+  setTimeout(()=>{
+    process.stdout.write(char);
+  },x);
+  x += 50;
+}
